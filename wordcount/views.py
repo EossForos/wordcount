e77a3a6a7fcb0adb0843b79502abeae35c03c622
EossForos <volosonopulos@gmail.com>
2020-03-05 20:49:43 +0200

    End

--- wordcount/views.py
+++ wordcount/views.py
@@ -25,3 +25,7 @@ def count(request):
     return render(request, 'count.html', {'fulltext': fulltext,
                                           'count': len(wordlist),
                                           'sortedwords': sortedwords})
+
+
+def about(request):
+    return render(request, 'about.html')
